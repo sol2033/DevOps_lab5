ce18258cee2e7882d9c7a63d4ba2f0c1cc0e4a38
sol2033 <sol2033@yandex.ru>
2026-03-22 12:23:53 +0300

    first commit

--- tests/test_user.py
+++ tests/test_user.py
@@ -26,16 +26,29 @@ def test_get_existed_user():
 
 def test_get_unexisted_user():
     '''Получение несуществующего пользователя'''
-    pass
+    response = client.get("/api/v1/user", params={'email': 'someEmail@yandex.ru'})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found"}
 
 def test_create_user_with_valid_email():
     '''Создание пользователя с уникальной почтой'''
-    pass
+    new_user = {
+        'name': 'Aboba',
+        'email': 'coolEmail@yandex.ru'
+    }
+    response = client.post("/api/v1/user", json=new_user)
+    assert response.status_code == 201
+    assert isinstance(response.json(), int)
 
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
+    exist_user = {'name': 'SomeName', 'email': users[0]['email']}
+    response = client.post("/api/v1/user", json=exist_user)
+    assert response.status_code == 409
+    assert response.json() == {"detail": "User with this email already exists"}
+    
 
 def test_delete_user():
     '''Удаление пользователя'''
-    pass
+    response = client.delete("/api/v1/user", params={'email': users[1]['email']})
+    assert response.status_code == 204
